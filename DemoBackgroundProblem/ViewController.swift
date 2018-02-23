c5d856bfda4d1d0f84ef3128079c25392ec337bc
amit <amit.gupta@saffrontech.net>
2018-02-23 11:14:02 +0530

    Initial Commit

---
+++ DemoBackgroundProblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoBackgroundProblem
+//
+//  Created by saffron on 23/02/18.
+//  Copyright © 2018 saffron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
